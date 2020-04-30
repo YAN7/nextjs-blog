55d00a157d675e7c333d088e348b5b5e23edd439
YAN7 <904394373@qq.com>
2020-04-30 21:45:44 +0800

    修复list不显示内容的bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ function Home({ allPostsData, theme, classes, changeMode }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headMd}>
-        <p className="about">hi, I'm YAN7, a front end engineer!</p>
+        <p className="about">hi, I'm YAN7, a pround front end engineer!</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
